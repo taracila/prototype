c4b070a9c9dfd89f1dc95bc4f46e077e651f67ed
Eugen Taracila <taracilaeugen@gmail.com>
2015-06-02 12:44:12 +0300

    Initial commit.

---
+++ Gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
+  filter = require('gulp-filter'),
+  swig = require('gulp-swig'),
+  sass = require('gulp-ruby-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  prettify = require('gulp-html-prettify'),
+  reload = browserSync.reload,
+  src = {
+    scss: 'scss/**/*.scss',
+    css: 'css',
+    html: 'templates/**/*.twig',
+  };
+
+/**
+ * Start the BrowserSync Static Server + Watch files
+ */
+gulp.task('serve', ['sass', 'templates'], function () {
+
+  browserSync({
+    server: "./",
+    files: ["css/styles.css", src.html]
+  });
+
+
+
+  gulp.watch(src.scss, ['sass']);
+  gulp.watch(src.html, ['templates']);
+});
+
+
+/**
+ * Kick off the sass stream with source maps + error handling
+ */
+function sassStream() {
+  return sass('./scss', {
+    sourcemap: true,
+    style: 'expanded',
+    unixNewlines: true
+  })
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(autoprefixer({ browsers: ['> 5%', 'last 1 version']}))
+    .pipe(sourcemaps.write('./', {
+      includeContent: false,
+      sourceRoot: '/scss'
+    }));
+}
+
+
+/**
+ * Compile sass, filter the results, inject CSS into all browsers.
+ */
+gulp.task('sass', function () {
+  return sassStream()
+    .pipe(gulp.dest(src.css))
+    .pipe(filter("**/*.css"))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+
+/**
+ * Generate templates.
+ */
+gulp.task('templates', function () {
+  return gulp.src(src.html)
+    .pipe(swig({
+      load_json: false,
+      defaults: {
+        cache: false
+      }
+    }))
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
+    .pipe(gulp.dest('./'))
+    .on("end", reload);
+});
+
+gulp.task('default', ['serve']);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# prototype
-Prototype
+### Setting Up Gulp With LiveReload, Sass, and Other Tasks
+
+[Read the tutorial](http://help.nitrous.io/setting-up-gulp-with-livereload-and-sass/)
+
+### Test out the App
+
+Click this button to start hacking on the Gulp sample app on [Nitrous.IO](https://www.nitrous.io/?utm_source=github.com&utm_campaign=nitrous_gulp_sample_app&utm_medium=hackonnitrous) in a matter of seconds:
+
+[![Hack nitrous-examples/gulp-sample-app on Nitrous.IO](https://d3o0mnbgv6k92a.cloudfront.net/assets/hack-l-v1-3cc067e71372f6045e1949af9d96095b.png)](https://www.nitrous.io/hack_button?source=embed&runtime=nodejs&repo=nitrous-examples%2Fgulp-sample-app&file_to_open=README.md)
+
+### Setup
+
+Run the following commands in the Terminal below to prepare and run the sample app:
+
+1. `cd ~/workspace/gulp-sample-app`
+2. `npm install gulp -g`
+3. `gem install sass`
+4. `npm install`
+5. `gulp`
+
+Go to the "Preview Menu" and click "Port 4000"
